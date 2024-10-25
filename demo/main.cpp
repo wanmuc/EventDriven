5a5aa88af9e1cfc638f20b80d698be1100ed40d8
wanmuc <769535441@qq.com>
2024-10-25 17:46:39 +0800

    代码优化

--- demo/main.cpp
+++ demo/main.cpp
@@ -9,9 +9,17 @@ void MyTimer(EventLoop &event_loop) {
   event_loop.TimerStart(1000, MyTimer, ref(event_loop));
 }
 
+void AcceptClient(Event * event) {
+  int client_fd = accept(event->fd, nullptr, 0);
+  if (client_fd > 0) {
+    cout << "accept client_fd = " << client_fd << endl;
+  }
+}
+
 int main(int argc, char *argv[]) {
   EventLoop event_loop;
   event_loop.TimerStart(1000, MyTimer, ref(event_loop));
+  event_loop.TcpListenStart("127.0.0.1", 18888, AcceptClient);
   event_loop.Run();
   return 0;
 }
